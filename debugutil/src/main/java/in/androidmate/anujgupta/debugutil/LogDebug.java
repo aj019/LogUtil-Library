070d72253caf68c615e762d0e1a76c71b8435f3b
Anuj Gupta <junagupta19@gmail.com>
2017-12-29 00:26:26 +0530

    Initial Commit

---
+++ debugutil/src/main/java/in/androidmate/anujgupta/debugutil/LogDebug.java
@@ -0,0 +1,16 @@
+package in.androidmate.anujgupta.debugutil;
+
+import android.util.Log;
+
+/**
+ * Created by anujgupta on 29/12/17.
+ */
+
+public class LogDebug {
+
+    private static String TAG = "";
+
+    public static void d(String str){
+        Log.d(TAG,str);
+    }
+}
